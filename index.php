673a4e490c2fd98ddca55931be9d44973e2010eb
Michael Lu <mlu@e1z2r9p34.42.us.org>
2017-10-31 16:49:18 -0700

    Updated style

--- index.php
+++ index.php
@@ -16,12 +16,13 @@
 		<link rel="stylesheet" type="text/css" href="./style/scroll.css">
 	</head>
 
-	<body class="Bg(grey-6)"><br>
-		<div class="Bd(s,1px) Bdrus(5px) H(1000px) W(630px) M(a) Bgc(grey-5) botbg">
-			<div class="botheader">
-				<h1 class="Ta(c)">KIFT - Temporary Layout</h1>
+	<body class="Bg(black) Ff(m)"><br>
+<!-- 	<div class="Fl(l)"> <img src="./img/animated.gif"> </div> -->
+		<div class="D(b) Bd(s,1px) Bdc(#32cd32) Bdrus(5px) H(1000px) W(630px) M(a) botbg">
+			<div class="botheader D(b)">
+				<h1 class="Ta(c) C(#32cd32) D(b)">KIFT - Temporary Layout</h1>
 			</div>
-			<div class="style" type="style" name="style" id="style">
+			<div class="style D(b)" type="style" name="style" id="style">
 			<?php
 				$text = unserialize(file_get_contents('./database/text'));
 				$output = $text;
@@ -29,22 +30,22 @@
 				foreach ($output as $key => $out)
 				{
 					echo '<div class="usertalk">
-						<div class="talk-bubble tri-right round right-top talktextl">User: ' . $output[$key]['msg'] . '</div>
-						<img src="https://dhqbrvplips7x.cloudfront.net/webchat/1.0.23/agent-e202505f.png" class="br-100 ba h3 w3 dib" alt="avatar">
+						<div class="Bd(s,2px) Bdc(#32cd32) talk-bubble round talktextl C(#32cd32)">User: ' . $output[$key]['msg'] . '</div>
+						<img src="./img/user.png" class="Bd(s,2px) Bdc(#32cd32) br-100 ba h3 w3 dib" alt="avatar">
 						</div>';
 					echo '<div class="aitalk">
-						<img src="https://camo.githubusercontent.com/bb8cfd28cdd5e2f042db6a02af3882bf561207c6/687474703a2f2f692e696d6775722e636f6d2f504d75365834442e706e67" class="br-100 ba h3 w3 dib" alt="avatar">
-						<div class="talk-bubble tri-right round left-top talktextr">AI: ' . $output[$key]['response'] . '</div>
+						<img src="./img/goodboye.png" class="br-100 ba h3 w3 dib Bd(s,2px) Bdc(#32cd32)" alt="avatar">
+						<div class="Bd(s,2px) Bdc(#32cd32) talk-bubble round talktextr C(#32cd32)">AI: ' . $output[$key]['response'] . '</div>
 						</div>';
 				}
 				file_put_contents('./database/text', serialize($text));
 
 			?>
 			</div>
-			<div class="push Ta(c)">
+			<div class="D(b) push Ta(c)">
 				<form action="integrate.php">
-					<input class="Bdrus(10px)" type="text" id="text" name="text" placeholder="Test here" /> 
-					<input class="Bdrus(100%)" type="submit" id="submit" name="submit" value="Ask"/>
+					<input class="Bdrus(10px) C(#32cd32)" type="text" id="text" name="text" placeholder="Test here" /> 
+					<input class="Bdrus(100%) C(#32cd32)" type="submit" id="submit" name="submit" value="Ask"/>
 				</form>
 			</div>
 		</div>
